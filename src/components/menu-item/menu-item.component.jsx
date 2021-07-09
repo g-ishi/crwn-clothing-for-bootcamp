e0590537d9cceeafeb5e3b266d8786a51fba85c8
石井源一 <ishiigenichi@ishiigenichinoMacBook-Air.local>
2021-07-09 15:36:53 +0900

    add router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.component.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => {
     return (
-        < div className={`menu-item ${size}`}>
+        < div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             {/* 背景画像だけホバーしたときに大きくしたいので、要素として独立させる必要がある */}
             <div className="background-image"
                 style={
@@ -21,4 +22,5 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+// withRouterにより、MenuItemコンポーネントでhistory, location, matchが使用可能になる
+export default withRouter(MenuItem);
