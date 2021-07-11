92cce0c8b9475a40cae9dbd595517123eba85cfc
石井源一 <ishiigenichi@ishiigenichinoMacBook-Air.local>
2021-07-11 11:08:57 +0900

    add header, style header

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop' component={ShopPage}></Route>
+      {/* こうすることでHeaderは必ず毎回レンダリングされる */}
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
